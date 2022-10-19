0e89c3131b18235e95f784cbb2059dfc538ca21f
Farid Jalil <faridfachu@gmail.com>
2022-10-19 18:33:20 -0300

    operators

---
+++ code.js
@@ -0,0 +1,23 @@
+saludo = "¡hola pedro!";
+pregunta = " ¿como estas? ";
+
+frase = saludo + pregunta;
+document.write(frase);
+
+numero1 = 5;
+numero2 = 8;
+frase = "" + numero1 + numero2;
+document.write(frase);
+
+nombre = " lucas dalto";
+frase1 = ` soy ${nombre} y estoy caminando `;
+document.write(frase1);
+
+let numero3 = 23;
+let numero4 = 13;
+document.write(numero3 == numero4);
+
+// document.write(numero3 != numero4);
+
+
+
