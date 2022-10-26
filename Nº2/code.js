d3854dc7bfdcb53a5f2e97cffefc5706fb0ac762
Farid Jalil <faridfachu@gmail.com>
2022-10-26 18:29:45 -0300

    loops

--- Nº2/code.js
+++ Nº2/code.js
@@ -1,5 +1,5 @@
 let frutas = ["manzana", "banana", "pera", 5, 9, "pedro"];
-document.write(frutas[5]);
+document.write(frutas[5] + "<br>");
 
 
 let pc = {
@@ -14,9 +14,19 @@ let procesador = pc["procesador"]
 let ram = pc["ram"]
 let espacio = pc["espacio"]
 
-frase = `<br> el nombre de mi pc es: ${nombre} <br>
+frase = `el nombre de mi pc es: ${nombre} <br>
          el procesador de mi pc es: ${procesador} <br>
          la memoria ram es: ${ram} <br>
-         el espacio de memoria es: ${espacio}`;
+         el espacio de memoria es: ${espacio} <br>`;
 
 document.write(frase)
+
+
+let numero = 0;
+
+do{
+    document.write(numero + "<br>");
+    numero++;
+}
+
+while (numero <= 6)
